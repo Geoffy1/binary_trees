cdae370dbf3d87863b96ea93d576be43a78b0486
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2022-12-14 21:57:15 +0300

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -4,7 +4,7 @@
  * binary_tree_height - measures the height
  * @tree: tree to measure the height of
  *
- * Return height of the tree
+ * Return: height of the tree
  * or 0 if tree is NULL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
