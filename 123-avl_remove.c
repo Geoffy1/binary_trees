621e7533de4f36f20d9137b536a9ed4909e87418
Geoffy1 <106971868+Geoffy1@users.noreply.github.com>
2022-12-14 22:24:30 +0300

    Create 123-avl_remove.c

---
+++ 123-avl_remove.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * avl_remove - removes a node
+ * @root: a pointer to the root node of the tree for removing a node
+ * @value: the value
+ *
+ * Return: a pointer to the new root node
+ * or NULL on failure
+ */
+avl_t *avl_remove(avl_t *root, int value)
+{
+
+}
